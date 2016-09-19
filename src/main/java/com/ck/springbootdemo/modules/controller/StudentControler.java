438ca542a35b5248628d889556bc9a730e82e31c
dudy <chenkai1792463586@gmail.com>
2016-09-19 21:58:51 +0800

    it supports freemarker :)

--- src/main/java/com/ck/springbootdemo/modules/controller/StudentControler.java
+++ src/main/java/com/ck/springbootdemo/modules/controller/StudentControler.java
@@ -2,22 +2,28 @@ package com.ck.springbootdemo.modules.controller;
 
 import java.util.Map;
 
+import com.ck.springbootdemo.modules.mapper.StudentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.common.collect.Maps;
 import com.google.gson.Gson;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentControler {
 
+	@Autowired
+	private StudentMapper studentMapper;
+
 	@RequestMapping("/index")
 	@ResponseBody
-	public String list (){
-		Map<String,String> map = Maps.newHashMap();
-		map.put("json", "hello");
-		Gson gson = new Gson();
-		return gson.toJson(map);
+	public ModelAndView index (){
+		ModelAndView mav = new ModelAndView("index");
+		mav.addObject("name", studentMapper.selectAll().get(0).getName());
+		return mav;
 	}
+
 }
